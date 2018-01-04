1450a67cab54aeae30176058c6d2935a94f78fbc
tmaolin <tianmaolin>
2018-01-04 21:48:47 +0800

    Initial commit

---
+++ src/main/java/com/tml/springconsumer/SpringConsumerApplication.java
@@ -0,0 +1,39 @@
+package com.tml.springconsumer;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.tml.springconsumer.service.KafkaConsumerServer;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.config.ContainerProperties;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.tml.springconsumer.dao")
+public class SpringConsumerApplication {
+
+	@Autowired
+	KafkaConsumerServer kafkaConsumerServer;
+
+	@Bean
+	ContainerProperties containerProperties() {
+		ContainerProperties topic1 = new ContainerProperties("topic1");
+		topic1.setMessageListener(kafkaConsumerServer);
+		return topic1;
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "datasource")
+	DataSource dataSource() {
+		return new DruidDataSource();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsumerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/tml/springconsumer/service/KafkaConsumerServer.java
@@ -0,0 +1,27 @@
+package com.tml.springconsumer.service;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumerServer implements MessageListener<String,String>{
+
+
+    @Override
+    public void onMessage(ConsumerRecord<String, String> record) {
+        System.out.println("KafkaConsumerServer=============kafkaConsumer开始消费=============");
+        String topic = record.topic();
+        String key = record.key();
+        String value = record.value();
+        long offset = record.offset();
+        int partition = record.partition();
+        System.out.println("KafkaConsumerServer-------------topic:"+topic);
+        System.out.println("KafkaConsumerServer-------------value:"+value);
+        System.out.println("KafkaConsumerServer-------------key:"+key);
+        System.out.println("KafkaConsumerServer-------------offset:"+offset);
+        System.out.println("KafkaConsumerServer-------------partition:"+partition);
+        System.out.println("~~~~~~~~~~~~~kafkaConsumer消费结束~~~~~~~~~~~~~");
+        System.out.println("消费成功***************************************************************");
+    }
+}
